ec610908d737d78a5b471a470df3c6c18d9b67e5
Claudia <claudia.wiese@sciencespo.fr>
2024-08-13 17:59:46 +0200

    add data_explore.py with draft sql requests

--- data_exploration/data_explore.py
+++ data_exploration/data_explore.py
@@ -1,19 +1,56 @@
 import sqlite3
 import os
 
-db_path = '../db/retail.db'
+db_path = 'db/retail.db'
 if not os.path.exists(db_path):
     print("Database file not found at:", db_path)
 else:
     print("Database file found.")
-
 conn = sqlite3.connect(db_path)
-
 cursor = conn.cursor()
 
-test = cursor.execute('''
-            SELECT * FROM transactions WHERE id = '04056f49-705b-491a-95e8-ab66a95d1ac4'
+print("QUESTIONS")
+print("-------------------------------------------------\n")  
+
+print("Question 1: What is the number of transactions on 14/01/2022 ?")
+question1 = cursor.execute('''
+            SELECT transaction_date, count(id) 
+            FROM transactions 
+            WHERE transaction_date = '2022-01-14'
+        ''').fetchone()
+
+print(f'The total number of transactions on 14/01/2022 is {question1}')
+
+print("-------------------------------------------------\n")  
+
+print("Question 2: What is the total amount, including tax, of all SELL transactions ?")
+question2 = cursor.execute('''
+            SELECT sum(amount_inc_tax)
+            FROM transactions 
+            WHERE category = 'SELL'
+        ''').fetchone()
+
+print(f'the total amount, including tax, of all SELL transactions is {question2}')
+
+
+print("-------------------------------------------------\n")  
+
+print("Question 3: Consider the product Amazon Echo Dot:")
+print("What is the balance (SELL - BUY) by date?")
+print("What is the cumulated balance (SELL - BUY) by date?")
+question3_1 = cursor.execute('''
+            SELECT sum(amount_inc_tax)
+            FROM transactions 
+            WHERE category = 'SELL'
         ''').fetchone()
     
-print(test)
+question3_2 = cursor.execute('''
+            SELECT sum(amount_inc_tax)
+            FROM transactions 
+            WHERE category = 'SELL'
+        ''')
+
+print(f'The balance (SELL - BUY) by date is {question3_1}')
+print(f'The cumulated balance (SELL - BUY) by date is {question3_2}')
+
 
